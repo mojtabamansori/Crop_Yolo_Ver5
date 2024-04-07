d9ee8b766b3cf329118886b77b246ff5bea3f578
mojtabamansori <13mrlien13@gmail.com>
2024-04-07 18:03:30 +0330

    update

--- yolo_crop_picture.py
+++ yolo_crop_picture.py
@@ -13,5 +13,5 @@ folder_path = r"F:\data reid"
 image_addresses = get_image_addresses(folder_path)
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 img = 'WhatsApp Image 2024-04-03 at 19.10.47.jpeg'
-results = model(image_addresses)
+results = model(img)
 crops = results.crop(save=True)
